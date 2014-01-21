4250306711d9cb16151595e8e7cc8fb376e76f6b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2014-01-21 14:05:43 -0500

    Adding the current version of the Team Assignment program. For teams.

--- teams/TeamAssignment.java
+++ teams/TeamAssignment.java
@@ -7,27 +7,35 @@ import java.util.Set;
 
 public class TeamAssignment 
 {
+	static final int TEAM_SIZE = 3;
+
     static final Map<Integer, String> STUDENTS = new HashMap<Integer, String>() {{
-	    put(1, "Ballinger");
-	    put(2, "Blake");
-	    put(3, "Challener");
-	    put(4, "Clive");
-	    put(5, "Doughty");
-	    put(6, "Graham");
-	    put(7, "Hajduk");
-	    put(8, "Harkema");
-	    put(9, "Jordan");
-	    put(10, "Kelly");
-	    put(11, "King");
-	    put(12, "Kotelyanskii");
-	    put(13, "Landgrebe");
-	    put(14, "MacMillan");
-	    put(15, "Mong");
-	    put(16, "Mukherjee");
-	    put(17, "Regel");
-	    put(18, "Wechter");
-	    put(19, "Weisman");
-	    put(20, "Weyant");
+	    put(1, "Barach");
+	    put(2, "Brumagin");
+	    put(3, "Choffel");
+	    put(4, "Christiansen");
+	    put(5, "Craft");
+	    put(6, "Dinga");
+	    put(7, "Ginoza");
+	    put(8, "Hanko");
+	    put(9, "Hawk");
+	    put(10, "Kineer");
+	    put(11, "Koo");
+	    put(12, "Lange");
+	    put(13, "Lawrence");
+	    put(14, "Livingston");
+	    put(15, "McCool");
+	    put(16, "McCurdy");
+	    put(17, "McIntyre");
+	    put(18, "Mulvay");
+	    put(19, "Nelson");
+	    put(20, "Ocampo");
+	    put(21, "Person");	
+	    put(22, "Popey");
+	    put(23, "Schweitzer");
+	    put(24, "Smith");
+	    put(25, "Tang");	
+	    put(26, "Zheng");	
 	}};
 
     public static void main(String[] args) {
@@ -35,7 +43,7 @@ public class TeamAssignment
 	ArrayList<Integer> students = new ArrayList<Integer>();
 
 	// populate the arraylist with the integers
-	for(int i = 1; i <= 20; i++) {
+	for(int i = 1; i <= 26; i++) {
 	    Integer I = new Integer(i);
 	    students.add(I);
 	}
@@ -60,7 +68,7 @@ public class TeamAssignment
 	int team = 1;
 	for(Integer studentToday : studentsTodaysOrdering) {
 
-		if(i % 5 == 0) {
+		if(i % TEAM_SIZE == 0) {
 		
 			System.out.println();
 			System.out.println("Team " + team);
